df30291d298dc608d98ee2e0362773db2bf11304
Sourabh_Bakshi <bakshisourabh1l@gmail.com>
2025-01-28 16:53:38 +0530

    updated

---
+++ test.js
@@ -0,0 +1,8 @@
+// var button = document.querySelector('.button');
+
+// button.addEventListener('touchstart',function(){
+//     button.style.backgroundColor = 'red';
+// })
+// button.addEventListener('touchend',function(){
+//     button.style.backgroundColor = '';
+// })
